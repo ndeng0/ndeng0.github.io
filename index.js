9d51ad6b6efae7f422989865cf96c5270324e6ab
ndeng0 <47835297+ndeng0@users.noreply.github.com>
2019-02-26 17:28:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,42 +21,43 @@ function _programming() {
   var ctx = canvas.getContext("2d");
 
   // Bottom line
-  line(ctx, 101, 320, 600, 320);
+  line(ctx, 201, 320, 700, 320);
   // Divide lines
-  line(ctx, 101, 0, 101, 320);
-  line(ctx, 200, 0, 200, 320);
+  line(ctx, 201, 0, 201, 320);
   line(ctx, 300, 0, 300, 320);
   line(ctx, 400, 0, 400, 320);
   line(ctx, 500, 0, 500, 320);
+  line(ctx, 600, 0, 600, 320);
 
   // Settings for categories text
-  ctx.font = "10px";
+  ctx.font = "20px Ariel";
   ctx.fillStyle = "black";
   ctx.textAlign = "right";
 
   // Categories
-  ctx.fillText("Java", 90, 40);
-  ctx.fillText("Python", 90, 120);
-  ctx.fillText("Javascript", 90, 200);
-  ctx.fillText("HTML/CSS", 90, 280);
+  ctx.fillText("Java", 190, 40);
+  ctx.fillText("Python", 190, 120);
+  ctx.fillText("Javascript", 190, 200);
+  ctx.fillText("HTML/CSS", 190, 280);
 
   // Levels
+  ctx.font = "15px Ariel"
   ctx.textAlign = "center";
-  ctx.fillText("Nothing", 100, 340);
-  ctx.fillText("Something", 200, 340);
-  ctx.fillText("OK", 300, 340);
-  ctx.fillText("Pretty Good", 400, 340);
-  ctx.fillText("Ask Me Anything", 500, 340);
+  ctx.fillText("Nothing", 200, 340);
+  ctx.fillText("Something", 300, 340);
+  ctx.fillText("OK", 400, 340);
+  ctx.fillText("Pretty Good", 500, 340);
+  ctx.fillText("Ask Me Anything", 600, 340);
 
   // Bars
   ctx.fillStyle = "green";
-  ctx.fillRect(100,20,300,40);
+  ctx.fillRect(200,20,300,40);
   ctx.fillStyle = "green";
-  ctx.fillRect(100,100,300,40);
+  ctx.fillRect(200,100,300,40);
   ctx.fillStyle = "blue";
-  ctx.fillRect(100,180,200,40);
+  ctx.fillRect(200,180,200,40);
   ctx.fillStyle = "red";
-  ctx.fillRect(100,260,100,40);
+  ctx.fillRect(200,260,100,40);
 }
 
 function _library() {
@@ -64,42 +65,43 @@ function _library() {
   var ctx = canvas.getContext("2d");
 
   // Bottom line
-  line(ctx, 101, 320, 600, 320);
+  line(ctx, 201, 320, 700, 320);
   // Divide lines
-  line(ctx, 101, 0, 101, 320);
-  line(ctx, 200, 0, 200, 320);
+  line(ctx, 201, 0, 201, 320);
   line(ctx, 300, 0, 300, 320);
   line(ctx, 400, 0, 400, 320);
   line(ctx, 500, 0, 500, 320);
+  line(ctx, 600, 0, 600, 320);
 
   // Settings for categories text
-  ctx.font = "10px";
+  ctx.font = "20px Ariel";
   ctx.fillStyle = "black";
   ctx.textAlign = "right";
 
   // Categories
-  ctx.fillText("Processing", 90, 40);
-  ctx.fillText("P5.js", 90, 120);
-  ctx.fillText("NumPy", 90, 200);
-  ctx.fillText("Jquery", 90, 280);
+  ctx.fillText("Processing", 190, 40);
+  ctx.fillText("P5.js", 190, 120);
+  ctx.fillText("NumPy", 190, 200);
+  ctx.fillText("Jquery", 190, 280);
 
   // Levels
+  ctx.font = "15px Ariel"
   ctx.textAlign = "center";
-  ctx.fillText("Nothing", 100, 340);
-  ctx.fillText("Something", 200, 340);
-  ctx.fillText("OK", 300, 340);
-  ctx.fillText("Pretty Good", 400, 340);
-  ctx.fillText("Ask Me Anything", 500, 340);
+  ctx.fillText("Nothing", 200, 340);
+  ctx.fillText("Something", 300, 340);
+  ctx.fillText("OK", 400, 340);
+  ctx.fillText("Pretty Good", 500, 340);
+  ctx.fillText("Ask Me Anything", 600, 340);
 
   // Bars
   ctx.fillStyle = "green";
-  ctx.fillRect(100,20,300,40);
+  ctx.fillRect(200,20,300,40);
   ctx.fillStyle = "blue";
-  ctx.fillRect(100,100,200,40);
+  ctx.fillRect(200,100,200,40);
   ctx.fillStyle = "blue";
-  ctx.fillRect(100,180,200,40);
+  ctx.fillRect(200,180,200,40);
   ctx.fillStyle = "red";
-  ctx.fillRect(100,260,100,40);
+  ctx.fillRect(200,260,100,40);
 }
 
 function _game() {
@@ -107,39 +109,40 @@ function _game() {
   var ctx = canvas.getContext("2d");
 
   // Bottom line
-  line(ctx, 101, 240, 600, 240);
+  line(ctx, 201, 240, 700, 240);
   // Divide lines
-  line(ctx, 101, 0, 101, 240);
-  line(ctx, 200, 0, 200, 240);
+  line(ctx, 201, 0, 201, 240);
   line(ctx, 300, 0, 300, 240);
   line(ctx, 400, 0, 400, 240);
   line(ctx, 500, 0, 500, 240);
+  line(ctx, 600, 0, 600, 240);
 
   // Settings for categories text
-  ctx.font = "10px";
+  ctx.font = "20px Ariel";
   ctx.fillStyle = "black";
   ctx.textAlign = "right";
 
   // Categories
-  ctx.fillText("Ren'Py", 90, 40);
-  ctx.fillText("Blender", 90, 120);
-  ctx.fillText("GameMaker Studio", 90, 200);
+  ctx.fillText("Ren'Py", 190, 40);
+  ctx.fillText("Blender", 190, 120);
+  ctx.fillText("GameMaker Studio", 190, 200);
 
   // Levels
+  ctx.font = "15px Ariel"
   ctx.textAlign = "center";
-  ctx.fillText("Nothing", 100, 260);
-  ctx.fillText("Something", 200, 260);
-  ctx.fillText("OK", 300, 260);
-  ctx.fillText("Pretty Good", 400, 260);
-  ctx.fillText("Ask Me Anything", 500, 260);
+  ctx.fillText("Nothing", 200, 260);
+  ctx.fillText("Something", 300, 260);
+  ctx.fillText("OK", 400, 260);
+  ctx.fillText("Pretty Good", 500, 260);
+  ctx.fillText("Ask Me Anything", 600, 260);
 
   // Bars
   ctx.fillStyle = "blue";
-  ctx.fillRect(100,20,200,40);
+  ctx.fillRect(200,20,200,40);
   ctx.fillStyle = "red";
-  ctx.fillRect(100,100,100,40);
+  ctx.fillRect(200,100,100,40);
   ctx.fillStyle = "blue";
-  ctx.fillRect(100,180,200,40);
+  ctx.fillRect(200,180,200,40);
 }
 
 function line(ctx, sx, sy, ex, ey) {
