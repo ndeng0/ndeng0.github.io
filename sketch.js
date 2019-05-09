89c150a79aa6ce40f9b10d81739b59725e5bab83
ndeng0 <47835297+ndeng0@users.noreply.github.com>
2019-05-09 00:50:28 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ let hp_up;
 let heal_hp;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight-200);
+  createCanvas(windowWidth, windowHeight-400);
   noCursor();
 
   player = new Player(width/2, height/2);
